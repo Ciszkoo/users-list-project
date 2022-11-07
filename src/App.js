7793cc6b0ccfed0cb14892692f1253bcc480f758
Ciszkoo <pawel.olszko.p@gmail.com>
2022-11-07 20:28:30 +0100

    implemented logic responsible for rendering list of users

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
-  const [users, setUsers] = useState([])
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList(() => {
+      return [
+        ...usersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
 
   return (
     <div>
-      <AddUser />
-      <UsersList users={users}/>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
